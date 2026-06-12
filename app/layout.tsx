954c8f0ba8a1f94b7d219c869c88428203fdb4a2
athenashopvn888 <athenashopvn888@gmail.com>
2026-06-12 15:51:12 +0700

    add google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -148,6 +148,17 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TNWC4CGQ1M"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TNWC4CGQ1M');
+            `
+          }}
+        />
       </head>
       <body>
         {children}
